9293018fdc235b72025e8f78af802f9e88f8f150
SiobhanP1 <siobhanphillips11@gmail.com>
2023-02-01 23:56:26 +0000

    Add function for updating question number to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,17 +26,20 @@ nextQ.addEventListener('click', nextQuestion);
 let questionIndex = 0;
 
 function displayQuestion() {
-    alert('displayQcalled');
-    let currentQuestion = document.getElementById('current-question');
-    let optionOne = document.getElementById('option-one-text');
-    let optionTwo = document.getElementById('option-two-text');
-    let optionThree = document.getElementById('option-three-text');
+    if (questionIndex > questions.length) {
+        displayEndingMsg();
+    } else {
+        alert('displayQcalled');
+        let currentQuestion = document.getElementById('current-question');
+        let optionOne = document.getElementById('option-one-text');
+        let optionTwo = document.getElementById('option-two-text');
+        let optionThree = document.getElementById('option-three-text');
 
-    currentQuestion.innerText = questions[questionIndex].question;
-    optionOne.innerText = questions[questionIndex].option1;
-    optionTwo.innerText = questions[questionIndex].option2;
-    optionThree.innerText = questions[questionIndex].option3;
-
+        currentQuestion.innerText = questions[questionIndex].question;
+        optionOne.innerText = questions[questionIndex].option1;
+        optionTwo.innerText = questions[questionIndex].option2;
+        optionThree.innerText = questions[questionIndex].option3;
+    }
 }
 
 /* Check answer correct or not*/
@@ -48,7 +51,6 @@ function checkAnswer() {
 
     if (document.getElementById('option-one').checked) {
         alert('option 1 selected');
-        /*let userSelected = document.getElementById('option-one-text').innerText;*/
         let userSelected = questions[questionIndex].option1;
 
         if (userSelected === questions[questionIndex].correctAnswer) {
@@ -56,28 +58,25 @@ function checkAnswer() {
         } else if (userSelected !== questions[questionIndex].correctAnswer) {
             incrementIncorrect();
         }
-        document.getElementById('next-q-button').classList.remove('hidden');
 
     } else if (document.getElementById('option-two').checked) {
         alert('option 2 selected');
         let userSelected = document.getElementById('option-two-text').innerText;
 
-        if (userSelected === questions[questionIndex].correctAnswer.value) {
+        if (userSelected === questions[questionIndex].correctAnswer) {
             incrementCorrect();
         } else {
             incrementIncorrect();
         }
-        document.getElementById('next-q-button').classList.remove('hidden');
 
     } else if (document.getElementById('option-three').checked) {
         let userSelected = document.getElementById('option-three-text').innerText;
 
-        if (userSelected === questions[questionIndex].correctAnswer.value) {
+        if (userSelected === questions[questionIndex].correctAnswer) {
             incrementCorrect();
         } else {
             incrementIncorrect();
         }
-        document.getElementById('next-q-button').classList.remove('hidden');
     } else {
         alert('Please choose an option.');
     }
@@ -88,30 +87,47 @@ function checkAnswer() {
 function incrementCorrect() {
     let correctAnswers = parseInt(document.getElementById('current-total-correct').innerHTML);
     document.getElementById('current-total-correct').innerText = correctAnswers + 1;
+    displayCorrect();
 }
 
 function incrementIncorrect() {
     let incorrectAnswers = parseInt(document.getElementById('current-total-incorrect').innerHTML);
     document.getElementById('current-total-incorrect').innerText = incorrectAnswers + 1;
+    displayIncorrect();
 }
 
 /* Display answer messages */
 function displayCorrect() {
     document.getElementById('correct').classList.remove('hidden');
+    document.getElementById('next-q-button').classList.remove('hidden');
 }
 
 function displayIncorrect() {
     document.getElementById('incorrect').classList.remove('hidden');
+    document.getElementById('next-q-button').classList.remove('hidden');
 }
 
 /**Hide displayed answer messages, check answer button and clear radio boxes*/
 function nextQuestion() {
     document.getElementById('correct').classList.add('hidden');
     document.getElementById('incorrect').classList.add('hidden');
+    document.getElementById('next-q-button').classList.add('hidden');
     let radiobuttons = document.getElementsByName('answer-options');
-    for (i=0; i<radiobuttons.length; i++) {
-        radiobuttons[i].checked=false;
+    for (i = 0; i < radiobuttons.length; i++) {
+        radiobuttons[i].checked = false;
     }
     questionIndex++;
     displayQuestion();
+    updateQuestionNumber();
+}
+
+function updateQuestionNumber() {
+    let currentQNumber = parseInt(document.getElementById('current-q-number').innerHTML);
+    if (currentQNumber < 5) {
+        document.getElementById('current-q-number').innerText = currentQNumber + 1;
+    }
+}
+
+function displayEndingMsg() {
+
 }
