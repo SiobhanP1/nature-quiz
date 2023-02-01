b08024e04833683b3370e7ac97df620093daaf68
SiobhanP1 <siobhanphillips11@gmail.com>
2023-02-01 17:44:56 +0000

    Add question and answer array to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+let questions = [
+    {'question': 'Question1',
+'option1': 'q1option1',
+'option2': 'q1option2',
+'option3': 'q1option3',
+'correctAnswer': 'correctAnsQ1'},
+{'question': 'question2',
+'option1': 'q2option1',
+'option2': 'q2option2',
+'option3': 'q2option3',
+'correctAnswer': 'correctAnsQ2'}
+];
+
