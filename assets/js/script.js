951820a647c28e41ec67bfa219b102a530c8874c
SiobhanP1 <siobhanphillips11@gmail.com>
2023-02-03 09:52:43 +0000

    Change variable names in script.js to remove abbreviations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,47 @@
 //Array containing quiz questions with corresponding answer options.
-let questions = [{
-        'question': 'Which bird is famously intelligent and known to remember humans that help it?',
-        'option1': 'Robin',
-        'option2': 'Thrush',
-        'option3': 'Crow',
-        'correctAnswer': 'Crow'
+const QUIZ_QUESTIONS = [{
+        "question": "Which bird is famously intelligent and known to remember humans that help it?",
+        "option1": "Robin",
+        "option2": "Thrush",
+        "option3": "Crow",
+        "correctAnswer": "Crow"
     },
     {
-        'question': 'There are 98 different species of which creature in Ireland?',
-        'option1': 'Birds',
-        'option2': 'Bees',
-        'option3': 'Cats',
-        'correctAnswer': 'Bees'
+        "question": "There are 98 different species of which creature in Ireland?",
+        "option1": "Birds",
+        "option2": "Bees",
+        "option3": "Cats",
+        "correctAnswer": "Bees"
     },
     {
-        'question': 'Question3?',
-        'option1': 'Birds',
-        'option2': 'Bees',
-        'option3': 'Cats',
-        'correctAnswer': 'Bees'
+        "question": "Question3?",
+        "option1": "Birds",
+        "option2": "Bees",
+        "option3": "Cats",
+        "correctAnswer": "Bees"
     },
     {
-        'question': 'Question4?',
-        'option1': 'Birds',
-        'option2': 'Bees',
-        'option3': 'Cats',
-        'correctAnswer': 'Bees'
+        "question": "Question4?",
+        "option1": "Birds",
+        "option2": "Bees",
+        "option3": "Cats",
+        "correctAnswer": "Bees"
     },
     {
-        'question': 'question5?',
-        'option1': 'Birds',
-        'option2': 'Bees',
-        'option3': 'Cats',
-        'correctAnswer': 'Bees'
+        "question": "Question5?",
+        "option1": "Birds",
+        "option2": "Bees",
+        "option3": "Cats",
+        "correctAnswer": "Bees"
     }
 ];
 
-let checkAns = document.getElementById('check-ans-button');
-let nextQ = document.getElementById('next-q-button');
+let checkAnswerButton = document.getElementById("check-answer-button");
+let nextQuestionButton = document.getElementById("next-question-button");
 
-document.addEventListener('DOMContentLoaded', displayQuestion);
-checkAns.addEventListener('click', checkAnswer);
-nextQ.addEventListener('click', nextQuestion);
+document.addEventListener("DOMContentLoaded", displayQuestion);
+checkAnswerButton.addEventListener("click", checkAnswer);
+nextQuestionButton.addEventListener("click", nextQuestion);
 
 let questionIndex = 0; //Indicates the index number of the current question object.
 
@@ -50,19 +50,19 @@ let questionIndex = 0; //Indicates the index number of the current question obje
 * array. 
 */
 function displayQuestion() {
-    if (questionIndex === questions.length) {
+    if (questionIndex === QUIZ_QUESTIONS.length) {
         displayEndingMsg();
     } else {
 
-        let currentQuestion = document.getElementById('current-question');
-        let optionOne = document.getElementById('option-one-text');
-        let optionTwo = document.getElementById('option-two-text');
-        let optionThree = document.getElementById('option-three-text');
+        let currentQuestion = document.getElementById("current-question");
+        let optionOne = document.getElementById("option-one-text");
+        let optionTwo = document.getElementById("option-two-text");
+        let optionThree = document.getElementById("option-three-text");
 
-        currentQuestion.innerText = questions[questionIndex].question;
-        optionOne.innerText = questions[questionIndex].option1;
-        optionTwo.innerText = questions[questionIndex].option2;
-        optionThree.innerText = questions[questionIndex].option3;
+        currentQuestion.innerText = QUIZ_QUESTIONS[questionIndex].question;
+        optionOne.innerText = QUIZ_QUESTIONS[questionIndex].option1;
+        optionTwo.innerText = QUIZ_QUESTIONS[questionIndex].option2;
+        optionThree.innerText = QUIZ_QUESTIONS[questionIndex].option3;
     }
 }
 
@@ -70,32 +70,32 @@ function displayQuestion() {
 * Checks the option selected by the user against the correct option in the questions array. 
 */
 function checkAnswer() {
-    if (document.getElementById('option-one').checked) {
-        document.getElementById('check-ans-button').classList.add('hidden');
-        let userSelected = questions[questionIndex].option1;
-        if (userSelected === questions[questionIndex].correctAnswer) {
+    if (document.getElementById("option-one").checked) {
+        document.getElementById("check-answer-button").classList.add("hidden");
+        let userSelected = QUIZ_QUESTIONS[questionIndex].option1;
+        if (userSelected === QUIZ_QUESTIONS[questionIndex].correctAnswer) {
             incrementCorrect();
-        } else if (userSelected !== questions[questionIndex].correctAnswer) {
+        } else if (userSelected !== QUIZ_QUESTIONS[questionIndex].correctAnswer) {
             incrementIncorrect();
         }
-    } else if (document.getElementById('option-two').checked) {
-        document.getElementById('check-ans-button').classList.add('hidden');
-        let userSelected = document.getElementById('option-two-text').innerText;
-        if (userSelected === questions[questionIndex].correctAnswer) {
+    } else if (document.getElementById("option-two").checked) {
+        document.getElementById("check-answer-button").classList.add("hidden");
+        let userSelected = document.getElementById("option-two-text").innerText;
+        if (userSelected === QUIZ_QUESTIONS[questionIndex].correctAnswer) {
             incrementCorrect();
         } else {
             incrementIncorrect();
         }
-    } else if (document.getElementById('option-three').checked) {
-        document.getElementById('check-ans-button').classList.add('hidden');
-        let userSelected = document.getElementById('option-three-text').innerText;
-        if (userSelected === questions[questionIndex].correctAnswer) {
+    } else if (document.getElementById("option-three").checked) {
+        document.getElementById("check-answer-button").classList.add("hidden");
+        let userSelected = document.getElementById("option-three-text").innerText;
+        if (userSelected === QUIZ_QUESTIONS[questionIndex].correctAnswer) {
             incrementCorrect();
         } else {
             incrementIncorrect();
         }
     } else {
-        alert('Please choose an option.');
+        alert("Please choose an option.");
     }
 }
 
@@ -103,8 +103,8 @@ function checkAnswer() {
  * Increments the correct answer score by 1. 
  */ 
 function incrementCorrect() {
-    let correctAnswers = parseInt(document.getElementById('current-total-correct').innerHTML);
-    document.getElementById('current-total-correct').innerText = correctAnswers + 1;
+    let correctAnswers = parseInt(document.getElementById("current-total-correct").innerHTML);
+    document.getElementById("current-total-correct").innerText = correctAnswers + 1;
     displayCorrect();
 }
 
@@ -112,8 +112,8 @@ function incrementCorrect() {
  * Increments the incorrect answer score by 1.
  */
 function incrementIncorrect() {
-    let incorrectAnswers = parseInt(document.getElementById('current-total-incorrect').innerHTML);
-    document.getElementById('current-total-incorrect').innerText = incorrectAnswers + 1;
+    let incorrectAnswers = parseInt(document.getElementById("current-total-incorrect").innerHTML);
+    document.getElementById("current-total-incorrect").innerText = incorrectAnswers + 1;
     displayIncorrect();
 }
 
@@ -121,8 +121,8 @@ function incrementIncorrect() {
  * Displays a correct answer message on submission of a correct answer.
  */
 function displayCorrect() {
-    document.getElementById('correct').classList.remove('hidden');
-    document.getElementById('next-q-button').classList.remove('hidden');
+    document.getElementById("correct").classList.remove("hidden");
+    document.getElementById("next-question-button").classList.remove("hidden");
 }
 
 /**
@@ -130,9 +130,9 @@ function displayCorrect() {
  * user on submission of an incorrect answer.
  */
 function displayIncorrect() {
-    document.getElementById('correct-answer').innerText = questions[questionIndex].correctAnswer;
-    document.getElementById('incorrect').classList.remove('hidden');
-    document.getElementById('next-q-button').classList.remove('hidden');
+    document.getElementById("correct-answer").innerText = QUIZ_QUESTIONS[questionIndex].correctAnswer;
+    document.getElementById("incorrect").classList.remove("hidden");
+    document.getElementById("next-question-button").classList.remove("hidden");
 }
 
 /**
@@ -140,16 +140,16 @@ function displayIncorrect() {
  * before a new question is displayed. 
  */
 function nextQuestion() {
-    document.getElementById('correct').classList.add('hidden');
-    document.getElementById('incorrect').classList.add('hidden');
-    document.getElementById('next-q-button').classList.add('hidden');
-    let radiobuttons = document.getElementsByName('answer-options');
+    document.getElementById("correct").classList.add("hidden");
+    document.getElementById("incorrect").classList.add("hidden");
+    document.getElementById("next-question-button").classList.add("hidden");
+    let radiobuttons = document.getElementsByName("answer-options");
     for (i = 0; i < radiobuttons.length; i++) {
         radiobuttons[i].checked = false;
     }
     questionIndex++;
     displayQuestion();
-    document.getElementById('check-ans-button').classList.remove('hidden');
+    document.getElementById("check-answer-button").classList.remove("hidden");
     updateQuestionNumber();
 }
 
@@ -157,9 +157,9 @@ function nextQuestion() {
  * Updates the displayed question number whenever a new question is displayed.
  */
 function updateQuestionNumber() {
-    let currentQNumber = parseInt(document.getElementById('current-q-number').innerHTML);
-    if (currentQNumber < 5) {
-        document.getElementById('current-q-number').innerText = currentQNumber + 1;
+    let currentQuestionNumber = parseInt(document.getElementById("current-question-number").innerHTML);
+    if (currentQuestionNumber < 5) {
+        document.getElementById("current-question-number").innerText = currentQuestionNumber + 1;
     }
 }
 
@@ -168,13 +168,13 @@ function updateQuestionNumber() {
  * to the final question and then displays a message with the user's score.
  */
 function displayEndingMsg() {
-    document.getElementById('quiz-section').classList.add('hidden');
-    document.getElementById('correct').classList.add('hidden');
-    document.getElementById('incorrect').classList.add('hidden');
-    document.getElementById('score-container').classList.add('hidden');
-    document.getElementById('next-q-button').classList.add('hidden');
-    document.getElementById('result').classList.remove('hidden');
+    document.getElementById("quiz-section").classList.add("hidden");
+    document.getElementById("correct").classList.add("hidden");
+    document.getElementById("incorrect").classList.add("hidden");
+    document.getElementById("score-container").classList.add("hidden");
+    document.getElementById("next-question-button").classList.add("hidden");
+    document.getElementById("result").classList.remove("hidden");
 
-    correctAnswers = parseInt(document.getElementById('current-total-correct').innerHTML);
-    document.getElementById('final-score').innerText = correctAnswers;
+    correctAnswers = parseInt(document.getElementById("current-total-correct").innerHTML);
+    document.getElementById("final-score").innerText = correctAnswers;
 }
